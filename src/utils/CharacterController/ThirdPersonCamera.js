837c3cef93c23557c9526088229ae959ee3bb714
Can Deniz <dcb.denizcan@gmail.com>
2021-01-20 15:25:40 +0300

    camera ctl

--- src/utils/CharacterController/ThirdPersonCamera.js
+++ src/utils/CharacterController/ThirdPersonCamera.js
@@ -17,6 +17,10 @@ export function ThirdPersonCamera(params) {
     this._currentLookat = new three.Vector3(); 
 
 
+    const POSITION_CONTROL = 0;
+    const ROTATION_CONTROL = 1;
+    this._numpadController = POSITION_CONTROL;
+
 
     const calcIdeal = function(paramVec) {
         const vec = new three.Vector3();
@@ -27,19 +31,43 @@ export function ThirdPersonCamera(params) {
     }.bind(this);
 
 
-    /*window.addEventListener("keypress", function(e) {
-        if(e.code === "NumpadSubtract") {
-            const dir = new three.Vector3();
-            this._camera.getWorldDirection(dir);
-            dir.applyQuaternion(new three.Quaternion().setFromAxisAngle(new three.Vector3(0,1,0), Math.PI * 0.5));
-            //this._lookat.applyQuaternion(new three.Quaternion().setFromAxisAngle(new three.Vector3(dir), Math.PI * 0.01));
-        } else if(e.code === "NumpadAdd") {
-            const dir = new three.Vector3();
-            this._camera.getWorldDirection(dir);
-            dir.applyQuaternion(new three.Quaternion().setFromAxisAngle(new three.Vector3(0,1,0), Math.PI * -0.5));
-            //this._lookat.applyQuaternion(new three.Quaternion().setFromAxisAngle(new three.Vector3(dir), Math.PI * 0.01));
+    window.addEventListener("keypress", function(e) {
+        if(e.code === "NumpadMultiply") {
+            this._numpadController++;
+            this._numpadController %= 2;
         }
-    }.bind(this), false);*/
+        else if(e.code === "NumpadSubtract") {
+            if(this._numpadController == POSITION_CONTROL) {
+                if(this._offset.z < -0.2)
+                    this._offset.add(new three.Vector3(0,0,0.1));
+            } 
+        } else if(e.code === "NumpadAdd") {
+            if(this._numpadController == POSITION_CONTROL) {
+                if(this._offset.z > -15)
+                    this._offset.add(new three.Vector3(0,0,-0.1));
+            } 
+        } else if(e.code === "Numpad8") {
+            if(this._numpadController == POSITION_CONTROL) {
+                this._offset.add(new three.Vector3(0,0.1,0));
+            } else {
+                this._lookat.applyQuaternion(new three.Quaternion().setFromAxisAngle(new three.Vector3(1,0,0), -0.1));
+            }
+        } else if(e.code === "Numpad2") {
+            if(this._numpadController == POSITION_CONTROL) {
+                this._offset.add(new three.Vector3(0,-0.1,0));
+            } else {
+                this._lookat.applyQuaternion(new three.Quaternion().setFromAxisAngle(new three.Vector3(1,0,0), 0.1));
+            }
+        } else if(e.code === "Numpad4") {
+            if(this._numpadController == POSITION_CONTROL) {
+                this._offset.add(new three.Vector3(0.1, 0, 0));
+            }
+        } else if(e.code === "Numpad6") {
+            if(this._numpadController == POSITION_CONTROL) {
+                this._offset.add(new three.Vector3(-0.1, 0, 0));
+            }
+        }
+    }.bind(this), false);
 
 
 
