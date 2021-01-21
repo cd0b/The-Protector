d1d735d853a33ffa496e6455d0bf194cd975e8e6
Bugra <abugra.183@hotmail.com>
2021-01-21 14:04:10 +0300

    ui
    Signed-off-by: Bugra <abugra.183@hotmail.com>

---
+++ src/utils/uitest.js
@@ -0,0 +1,5 @@
+import * as three from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r124/three.module.min.js';
+import {CharacterControllerProxy} from './CharacterControllerProxy.js';
+import {ChararacterControllerInput} from './CharacterControllerInput.js';
+import {CharacterFiniteStateMachine} from './CharacterFiniteStateMachine.js';
+import { glb } from '../../global/global.js';
